4fdff85860bbd81ba627f3ad678f1d2b3ee894a3
Morgan Benton <registered@morphatic.com>
2020-07-13 22:37:18 -0400

    chore(app): install Vue and Vuetify
    Installed Laravel UI, Vue and Vuetify and setup basic config

---
+++ resources/js/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      Fynapse
+    </v-app-bar>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+import Vue from 'vue'
+import vuetify from './plugins/vuetify'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify,
+  render: h => h(App),
+}).$mount('#app')
